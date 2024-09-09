1964b3eca1207ed5bd951952989fcbd119358b0b
Lauri Heiskanen <lauri.heiskanen@nimble.fi>
2024-09-09 22:51:46 +0300

    main func and codepoint lookup for encoding

--- tools/encode.py
+++ tools/encode.py
@@ -1,9 +1,12 @@
 import sys
+from html.entities import codepoint2name
 
 
 def encode(text):
-  return "".join([f"&#{cp};" for cp in [ord(c) for c in text]])
+  return "".join([f"&#{cp};" for cp in [codepoint2name[cp] if cp in codepoint2name else cp for cp in [ord(c) for c in text]]])
 
+def main():
+    print(encode(sys.argv[1]))
 
-print(encode(sys.argv[1]))
-
+if __name__ == '__main__':
+    main()

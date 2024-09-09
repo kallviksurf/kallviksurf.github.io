1bb6ee03238a5306b39a9b43865392f2d276c8d7
Lauri Heiskanen <lauri.heiskanen@nimble.fi>
2024-09-09 23:02:21 +0300

    codepoint name fix

--- tools/encode.py
+++ tools/encode.py
@@ -3,7 +3,7 @@ from html.entities import codepoint2name
 
 
 def encode(text):
-  return "".join([f"&#{cp};" for cp in [codepoint2name[cp] if cp in codepoint2name else cp for cp in [ord(c) for c in text]]])
+  return "".join(f"&{codepoint2name[cp]};" if cp in codepoint2name else f"&#{cp};" for cp in [ord(c) for c in text])
 
 def main():
     print(encode(sys.argv[1]))
